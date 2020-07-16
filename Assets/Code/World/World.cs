2e10489cf5d5bd06f049fece793018f35bd232cf
Hakan Ahmet Tekin <hakan.ahmet.tekin@gmail.com>
2020-07-16 12:48:33 +0300

    Correct movement. Chunk generation and destruction is working

--- Assets/Code/World/World.cs
+++ Assets/Code/World/World.cs
@@ -129,7 +129,7 @@ namespace Assets.Code.World
                     {  
                         c.MoveChunk(movementDelta);
                         //If chunk's rightmost position is now behind the player, then it cannot be seen again. Therefore it can be destoyed.
-                        if (c.transform.position.x + c.Size.x / 2 < player.transform.position.x + chunkRemovalPositionOffset)
+                        if (c.transform.position.x + c.Size.x / 2 < player.transform.position.x - chunkRemovalPositionOffset)
                             RemoveUnusedChunks(i);
                     }
                     if(timeDelta != 0f) //Time is being altered. Update chunk blippers
